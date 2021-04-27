181ce98ccc1b00e6b9ee5f61a3269018373d2a48
Matt Clarke <matt.clarke@esss.se>
2021-04-27 07:28:55 +0200

    Remove future!

--- nicos_ess/loki/gui/setup_exp.py
+++ nicos_ess/loki/gui/setup_exp.py
@@ -27,8 +27,6 @@
 
 """NICOS GUI experiment setup window."""
 
-from __future__ import absolute_import, division, print_function
-
 from nicos.clients.gui.panels import PanelDialog
 from nicos.clients.gui.utils import loadUi
 from nicos.core import ConfigurationError
