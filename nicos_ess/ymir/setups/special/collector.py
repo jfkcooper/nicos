691adb0b372774ac8fd61865457646f1c632f046
Matt Clarke <matt.clarke@esss.se>
2021-05-12 12:08:12 +0200

    Correct kafka settings for YMIR

--- nicos_ess/ymir/setups/special/collector.py
+++ nicos_ess/ymir/setups/special/collector.py
@@ -5,11 +5,11 @@ devices = dict(
     CacheKafka=device(
         'nicos_ess.devices.cache_kafka_forwarder.CacheKafkaForwarder',
         dev_ignore=['space', 'sample'],
-        brokers=['localhost:9092'],
-        output_topic='from_nicos',
+        brokers=['172.30.242.20:9092'],
+        output_topic='ymir_nicos_devices',
     ),
     Collector=device('nicos.services.collector.Collector',
-        cache='localhost:14869',
+        cache='172.30.242.20:14869',
         forwarders=['CacheKafka'],
     ),
 )
