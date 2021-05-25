829830fdf038f81e34ffedda5118373ec8e1a82d
AÜC Hardal <umit.hardal@ess.eu>
2021-05-25 10:05:51 +0200

    reverse tab

--- nicos_ess/loki/setups/system.py
+++ nicos_ess/loki/setups/system.py
@@ -13,29 +13,29 @@ modules = ['nicos.commands.standard', 'nicos_ess.loki.commands.scripting']
 
 devices = dict(
     LoKI=device('nicos.devices.instrument.Instrument',
-                description='instrument object',
-                instrument='LoKI',
-                responsible='J. Houston <judith.houston@ess.eu>',
-                website='https://europeanspallationsource.se/instruments/loki'
-                ),
+        description='instrument object',
+        instrument='LoKI',
+        responsible='J. Houston <judith.houston@ess.eu>',
+        website='https://europeanspallationsource.se/instruments/loki'
+    ),
 
     Sample=device('nicos_ess.loki.devices.sample.LokiSample',
-                  description='The currently used sample',
-                  ),
+        description='The currently used sample',
+    ),
 
     Exp=device('nicos.devices.experiment.Experiment',
-               description='experiment object',
-               dataroot='/opt/nicos-data',
-               sendmail=False,
-               serviceexp='p0',
-               sample='Sample',
-               ),
+        description='experiment object',
+        dataroot='/opt/nicos-data',
+        sendmail=False,
+        serviceexp='p0',
+        sample='Sample',
+    ),
 
     InstrumentSettings=device('nicos_ess.loki.devices.'
-                              'experiment_configuration.InstrumentSettings',
-                              description='aperture and offset settings',
-                              lowlevel=True,
-                              ),
+                    'experiment_configuration.InstrumentSettings',
+                    description='aperture and offset settings',
+                    lowlevel=True,
+    ),
 
     filesink=device('nicos.devices.datasinks.AsciiScanfileSink', ),
 
@@ -44,14 +44,14 @@ devices = dict(
     daemonsink=device('nicos.devices.datasinks.DaemonSink', ),
 
     Space=device('nicos.devices.generic.FreeSpace',
-                 description='The amount of free space for storing data',
-                 path=None,
-                 minfree=5,
-                 ),
+        description='The amount of free space for storing data',
+        path=None,
+        minfree=5,
+    ),
 
     positioner=device('nicos.devices.generic.DeviceAlias',
-                      devclass='nicos.core.device.Moveable',
-                      ),
+        devclass='nicos.core.device.Moveable',
+    ),
 )
 
 startupcode = '''
