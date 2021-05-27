8723ea4ee3819939255b3200aa23866bdcff77f8
Matt Clarke <matt.clarke@esss.se>
2021-05-27 15:45:06 +0200

    Added limits to the vinci pump

--- nicos_ess/labs/premp/setups/vinci_pump.py
+++ nicos_ess/labs/premp/setups/vinci_pump.py
@@ -17,6 +17,15 @@ devices = dict(
         'nicos_ess.devices.epics.pva.EpicsAnalogMoveable',
         description='The pressure',
         readpv='{}Pressure'.format(pv_root),
-        writepv='{}PM_Press_SP'.format(pv_root)
+        writepv='{}PM_Press_SP'.format(pv_root),
+        abslimits=(0, 200),
     ),
+    vinci_start=device(
+        'nicos_ess.devices.epics.pva.EpicsMappedMoveable',
+        description='Set the burster function',
+        readpv='{}Start_Cmd'.format(pv_root),
+        writepv='{}Start_Cmd'.format(pv_root),
+        lowlevel=True,
+    ),
+
 )
