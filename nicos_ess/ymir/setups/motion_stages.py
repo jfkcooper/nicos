de827f2c98ff1aafa21d6b756ec8aa2c43087168
Matt Clarke <matt.clarke@esss.se>
2021-07-06 06:59:17 +0000

    Update motion_stages.py

--- nicos_ess/ymir/setups/motion_stages.py
+++ nicos_ess/ymir/setups/motion_stages.py
@@ -4,9 +4,9 @@ devices = dict(
     m1=device('nicos_ess.devices.epics.pva.EpicsMotor',
               epicstimeout=3.0,
               description='Single axis positioner',
-              motorpv='IOC:m1',
-              # errormsgpv='LabS-ESSIIP:MC-MCU-01:m1-MsgTxt',
-              # errorbitpv='LabS-ESSIIP:MC-MCU-01:m1-Err',
-              # reseterrorpv='LabS-ESSIIP:MC-MCU-01:m1-ErrRst',
+              motorpv='LabS-ESSIIP:MC-MCU-01:m1',
+              errormsgpv='LabS-ESSIIP:MC-MCU-01:m1-MsgTxt',
+              errorbitpv='LabS-ESSIIP:MC-MCU-01:m1-Err',
+              reseterrorpv='LabS-ESSIIP:MC-MCU-01:m1-ErrRst',
               ),
 )
