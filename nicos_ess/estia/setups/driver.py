2b87f7c08bc3ac926afd5271ca6841a5a8b6a041
Jonas Petersson <jonas.petersson@ess.eu>
2023-04-26 10:25:55 +0200

    mm

--- nicos_ess/estia/setups/driver.py
+++ nicos_ess/estia/setups/driver.py
@@ -60,7 +60,7 @@ devices = dict(
         pva=True,
     ),
     driver1_1_hex_state=device(
-        'nicos.devices.epics.pva.EpicsMappedMoveable',
+        'nicos.devices.epics.pva.EpicsMappedReadable',
         description='Hexscrew state',
         readpv=f'{pvprefix}Mtr8-HexScrew',
         monitor=True,
@@ -68,7 +68,7 @@ devices = dict(
         pva=True,
     ),
     driver1_2_hex_state=device(
-        'nicos.devices.epics.pva.EpicsMappedMoveable',
+        'nicos.devices.epics.pva.EpicsMappedReadable',
         description='Hexscrew state',
         readpv=f'{pvprefix}Mtr9-HexScrew',
         monitor=True,
