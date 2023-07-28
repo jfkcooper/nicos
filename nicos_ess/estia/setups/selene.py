62d58faba52bfe9f07e7a8130388cc2bea2e7520
Matt Clarke <matt.clarke@ess.eu>
2023-07-28 17:57:20 +0200

    switch to active position file

--- nicos_ess/estia/setups/selene.py
+++ nicos_ess/estia/setups/selene.py
@@ -8,7 +8,7 @@ devices = dict(
     sr1=device(
         'nicos_ess.estia.devices.selene.SeleneRobot',
         description='Selene 1 Robot',
-        position_data='data_artur/selene1_roboit_refined.yaml',
+        position_data='data_artur/selene1_robot_active.yaml',
         engaged=0.02, retracted=27.98, delta12=358.7,
         move_x='robot_pos', move_z='robot_vert',
         adjust1='driver1_1_adjust', approach1='driver1_1_approach', hex_state1='driver1_1_hex_state',
