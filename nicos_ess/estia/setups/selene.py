18eba622706102c5b7cbbe06ac69c203584a7ac1
Artur Glavic <artur.glavic@psi.ch>
2023-08-03 22:14:53 +0100

    change fogotton items

--- nicos_ess/estia/setups/selene.py
+++ nicos_ess/estia/setups/selene.py
@@ -21,8 +21,8 @@ devices = dict(
         position_data='data_artur/selene2_robot_active.yaml',
         engaged=-0.02, retracted=-28.0, delta12=358.7,
         move_x='robot_pos', move_z='robot_vert',
-        adjust1='driver1_2_adjust', approach1='driver1_1_approach', hex_state1='driver1_1_hex_state',
-        adjust2='driver1_1_adjust', approach2='driver1_2_approach', hex_state2='driver1_2_hex_state',
+        adjust1='driver1_2_adjust', approach1='driver1_2_approach', hex_state1='driver1_2_hex_state',
+        adjust2='driver1_1_adjust', approach2='driver1_1_approach', hex_state2='driver1_1_hex_state',
         unit='Item/Group'
     ),
     sm2=device(
