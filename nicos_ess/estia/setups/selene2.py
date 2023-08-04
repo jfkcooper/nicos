87f0c89c6827a910795097fa503eddf2d75283b0
Artur Glavic <artur.glavic@psi.ch>
2023-08-04 19:37:28 +0200

    small fix to configuration

--- nicos_ess/estia/setups/selene2.py
+++ nicos_ess/estia/setups/selene2.py
@@ -9,7 +9,7 @@ devices = dict(
         'nicos_ess.estia.devices.selene.SeleneRobot',
         description='Selene 2 Robot',
         position_data='data_artur/selene2_robot_active.yaml',
-        engaged=-0.02, retracted=-28.0, delta12=358.7,
+        engaged=-0.1, retracted=-28.0, delta12=358.7,
         move_x='robot2_pos', move_z='robot2_vert',
         adjust1='driver2_2_adjust', approach1='driver2_2_approach', hex_state1='driver2_2_hex_state',
         adjust2='driver2_1_adjust', approach2='driver2_1_approach', hex_state2='driver2_1_hex_state',
@@ -18,7 +18,7 @@ devices = dict(
     sm2=device(
         'nicos_ess.estia.devices.selene.SeleneMetrology',
         description='Selene 2 Metrology', unit='',
-        m_cart='mcart', interferometer='multiline',
+        m_cart='mcart2', interferometer='multiline',
         ch_u_h1='ch19', ch_u_h2='ch20', ch_d_h1='ch23', ch_d_h2='ch24',
         ch_u_v1='ch17', ch_u_v2='ch18', ch_d_v1='ch21', ch_d_v2='ch22',
     )
