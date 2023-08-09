1891945c5ad2a40aea3a19866c27e13a44a99126
Artur Glavic <artur.glavic@psi.ch>
2023-08-09 13:26:07 +0200

    Fix position calculation and callibration of SeleneMetrology and remove out-dated setups

---
+++ nicos_ess/estia/setups/dummy_detector.py
@@ -0,0 +1,9 @@
+description = 'A dummy detector'
+
+devices = dict(
+    dummy_detector=device(
+            'nicos_ess.estia.devices.dummy_detector.DummyDetector',
+            description='A dummy detector',
+    )
+)
+
